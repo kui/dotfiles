f7f224659cfaff0ebcd865b42eec22346fbb4d22
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-04 23:44:44 +0900

    2011-09-04 23:44:44 ./syncsyncbox.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ main(){
     local curr_dir=`pwd`
 
     local run_flag=1
-    while getopts r:run: opt
+    while getopts opt
     do
         case $opt in
             r | run) local run_flag=0 ;;
