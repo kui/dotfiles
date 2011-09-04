ddc5a7e679aac1cbd5a9dc3d22f31da7ba6fd901
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-05 00:23:57 +0900

    2011-09-05 00:23:57 ./syncsyncbox.sh

--- syncsyncbox.sh
+++ syncsyncbox.sh
@@ -46,8 +46,11 @@ sigint_hook(){
 sync(){
     git add . 2>&1
     local dry_run=`commit --porcelain`
-    echo -n $dry_run
-    commit --quiet
+    if [ -n $dry_run ]
+    then
+        echo $dry_run
+        commit --quiet
+    fi
     git push --quiet 2>&1 | grep -v "^Everything up-to-date$"
     git pull --ff 2>&1 | grep -v "^Already up-to-date.$"
 }
