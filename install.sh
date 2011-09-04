c94023e840a41cfe31c1aa551426943012244b65
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-04 23:23:33 +0900

    2011-09-04 23:23:33 ./syncsyncbox.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # -*- coding:utf-8 -*-
 # install script for setting files
 
-files=(
-    dotemacs
-    dotscreenrc
-    dotemacs.d
-    dotzshrc
-)
+files="
+dotemacs
+dotscreenrc
+dotemacs.d
+dotzshrc
+"
 
 main(){
     local prev_dir=`pwd`
