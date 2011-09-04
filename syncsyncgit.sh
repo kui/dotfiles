fff5f31ea8add94e955a4728f8d08e5c4ac6eba6
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-05 01:47:24 +0900

    2011-09-05 01:47:24 ./syncsyncbox.sh

--- syncsyncgit.sh
+++ syncsyncgit.sh
@@ -43,13 +43,16 @@ run(){
         sleep $INTERVAL
         count=$[$count+1]
     done &
+
     pid=$1
+
     if [ $? -eq 0 ]
     then
         echo "OK"
         create_pid_file $!
     else
         echo "FALSE"
+        exit 1
     fi
     
 }
