ff77a5412a79250748e0eee7f436170d36d5f0a7
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-15 20:25:41 +0900

    2011-09-15 20:25:41 /Users/kui/.settings.git/.syncsyncgit.sh

--- update_prompt.sh
+++ update_prompt.sh
@@ -2,4 +2,5 @@ PROMPT=$'\n'$PromptUserColor'${USER}@${HOST} '$Yellow'%~ '$'\n'$Default'%(!.#.$)
 RPROMPT=$DarkGray'%D [ %T ] '$Default
 
 update_prompt(){
+    local datetime=`date +'%Y/%m/%d %H:%M:%S'`
 }
