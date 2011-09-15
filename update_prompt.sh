beda9821a9a120470c0a4e6445da71455fd94a6a
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-15 20:57:05 +0900

    2011-09-15 20:57:05 /Users/kui/.settings.git/.syncsyncgit.sh

--- update_prompt.sh
+++ update_prompt.sh
@@ -13,7 +13,8 @@ update_prompt(){
 
     while [ num_bar -gt 0 ]
     do
-        bar="$bar="
+        local bar="$bar="
+        local num_bar=$((${num_bar}-1))
     done
     PROMPT="$left "$'\n'"$Default\$ "
 }
