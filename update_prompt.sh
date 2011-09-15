16f79dd2d763af34f085dc7d7c209cb558b31f63
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-15 20:43:53 +0900

    2011-09-15 20:43:53 /Users/kui/.settings.git/.syncsyncgit.sh

--- update_prompt.sh
+++ update_prompt.sh
@@ -5,5 +5,6 @@ update_prompt(){
     local datetime="`date +'%Y/%m/%d %H:%M:%S'`"
     local escaped_home="`echo ${HOME}|sed -e 's/\//\\\\\//g'`"
     local current_path="`pwd|sed -e \"s/^${escaped_home}/~/\"`"
+    local num_bar=
     PROMPT="$current_path "$'\n'"$Default\$ "
 }
