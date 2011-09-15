a7e60869b26b74eed77f879c1f721b1fcf531601
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-15 21:20:25 +0900

    2011-09-15 21:20:25 /Users/kui/.settings.git/.syncsyncgit.sh

--- update_prompt.sh
+++ update_prompt.sh
@@ -11,11 +11,11 @@ update_prompt(){
     local num_bar=`print -n -P -- "$left$right" | sed -e $'s/\e\[[0-9;]*m//g' | wc -m | sed -e 's/ //g'`
     local num_bar=$((${COLUMNS}-${num_bar}))
 
-    local sep=" -"
+    local sep=" .*"
     local bar=""
-    while [ $num_bar -gt ${#bar} ]
+    while [ $num_bar -gt $((${#bar}+${#sep})) ]
     do
-        local bar="$bar -"
+        local bar="$bar$sep"
     done
 
     PROMPT=$'\n'"${left}"$'%{\e[1;30m%}'"${bar}${right}"$'\n'"${Default}\$ "
