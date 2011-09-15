12ff59dfe2f64946712e775fddc7c7b97c45b41f
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-15 20:38:50 +0900

    2011-09-15 20:38:50 /Users/kui/.settings.git/.syncsyncgit.sh

--- update_prompt.sh
+++ update_prompt.sh
@@ -2,8 +2,8 @@ PROMPT=$'\n'$PromptUserColor'${USER}@${HOST} '$Yellow'%~ '$'\n'$Default'%(!.#.$)
 RPROMPT=$DarkGray'%D [ %T ] '$Default
 
 update_prompt(){
-    local datetime=`date +'%Y/%m/%d %H:%M:%S'`
-    local escaped_home=`echo ${HOME}|sed -e 's/\//\\\//g'`
-    local current_path=`pwd|sed -e "s/^${escaped_home}/~/"`
+    local datetime="`date +'%Y/%m/%d %H:%M:%S'`"
+    local escaped_home="`echo ${HOME}|sed -e 's/\//\\\//g'`"
+    local current_path="`pwd|sed -e "s/^${escaped_home}/~/"`"
     PROMPT="$current_path\n$ "
 }
