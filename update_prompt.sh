f74f95e52de9d0e7349e0576a22b80b1608193a2
Keiichiro Ui <keiichiro.ui@gmail.com>
2011-09-15 21:26:30 +0900

    2011-09-15 21:26:30 /Users/kui/.settings.git/.syncsyncgit.sh

--- update_prompt.sh
+++ update_prompt.sh
@@ -6,12 +6,12 @@ update_prompt(){
     local escaped_home="`echo ${HOME}|sed -e 's/\//\\\\\//g'`"
     local current_path="`pwd|sed -e \"s/^${escaped_home}/~/\"`"
 
-    local left=$'%{\e[1;36m%}'"${USER}@${HOST}${Default}:"$'%{\e[1;33m%}'"${current_path}  "
-    local right=$'%{\e[0;37m%}'"  ${datetime}"
+    local left=$'%{\e[1;36m%}'"${USER}@${HOST}${Default}:"$'%{\e[1;33m%}'"${current_path} "
+    local right=$'%{\e[0;37m%}'" ${datetime}"
     local num_bar=`print -n -P -- "$left$right" | sed -e $'s/\e\[[0-9;]*m//g' | wc -m | sed -e 's/ //g'`
     local num_bar=$((${COLUMNS}-${num_bar}))
 
-    local sep=" * "
+    local sep=" *"
     local bar=""
     while [ $num_bar -gt $((${#bar}+${#sep})) ]
     do
