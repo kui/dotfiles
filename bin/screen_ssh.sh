2f349c42919940e5825d1b219872958312e0c46c
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-06-26 17:23:55 +0900

    全面的に書き換え

--- bin/screen_ssh.sh
+++ bin/screen_ssh.sh
@@ -1,71 +1,46 @@
-#!/bin/bash
-# screen_ssh.sh by Chris Jones <cmsj@tenshu.net>
-# Released under the GNU GPL v2 licence.
-# Set the title of the current screen to the hostname being ssh'd to
+#!/bin/sh -eu
+# -*- coding:utf-8-unix; mode:sh; -*-
+# usage: screen_ssh.sh pid hostname username
 #
-# usage: screen_ssh.sh $PPID hostname
+# .ssh/config の最初の行に下記の三行を追加
+#	Host *
+#	PermitLocalCommand yes
+#	LocalCommand /path/to/screen_ssh.sh $PPID %n %r
 #
-# This is intended to be called by ssh(1) as a LocalCommand.
-# For example, put this in ~/.ssh/config:
-#
-# Host *
-#   LocalCommand /path/to/screen_ssh.sh $PPID %n
 
-# If it's not working and you want to know why, set DEBUG to 1 and check the
-# logfile.
-DEBUG=0
-DEBUGLOG="$HOME/.ssh/screen_ssh.log"
+# 0以外: デバッグ出力する
+# 0    : 出力しない
+DEBUG=1
 
-set -e
-set -u
-
-dbg ()
-{
-  if [ "$DEBUG" -gt 0 ]; then
-    echo "$(date) :: $*" >>$DEBUGLOG
-  fi
+d() {
+    [ $DEBUG != 0 ] && (echo "$@" | tee $HOME/.ssh/screen_ssh.sh)
+}
+a() {
+    d "abort: $@" >&2
+    exit 1
 }
 
-dbg "$0 $*"
+# 端末上で実行されてる？
+! tty -s && a "stdin is not a terminal"
+# [ ! -t 0 ] && a "stdin is not a terminal"
 
-# We only care if we are in a terminal
-tty -s
+# screen 上で実行されてる？
+[ -z "$STY" ] && a "not in screen"
 
-# We also only care if we are in screen, which we infer by $TERM starting
-# with "screen"
-if [ "${TERM:0:6}" != "screen" ]; then
-  dbg "Not a screen session, ${TERM:0:5} != 'screen'"
-  exit
-fi
+# 引数の数は正しい？
+[ $# != "3" ] && a "invalid args"
 
-# We must be given two arguments - our parent process and a hostname
-# (which may be "%n" if we are being called by an older SSH)
-if [ $# != "2" ]; then
-  dbg "Not given enough arguments (must have PPID and hostname)"
-  exit
-fi
+# SSH は対話モードで実行されてる？（バッチモードで実行されてない？）
+grep -a -i "Batchmode yes" /proc/$1/cmdline >/dev/null 2>&1 && a "SSH is in Batch mode"
 
-# We don't want to do anything if BatchMode is on, since it means
-# we're not an interactive shell
-set +e
-grep -a -i "Batchmode yes" /proc/$1/cmdline >/dev/null 2>&1
-RETVAL=$?
-if [ "$RETVAL" -eq "0" ]; then
-  dbg "SSH is being used in Batch mode, exiting because this is probably an auto-complete or similar"
-  exit
-fi
-set -e
+HOST="$2"
+USER="$3"
+d "host:	$HOST"
+d "user:	$USER"
 
-# Infer which version of SSH called us, and use an appropriate method
-# to find the hostname
-if [ "$2" = "%n" ]; then
-  HOST=$(xargs -0 < /proc/$1/cmdline)
-  dbg "Using OpenSSH 4.x hostname guess: $HOST"
-else
-  HOST="$2"
-  dbg "Using OpenSSH 5.x hostname specification: $HOST"
-fi
+# タイトルの名前どうする？
+TITLE="$USER@$HOST"
+d "title:	$TITLE"
 
-echo $HOST | sed -e 's/\.[^.]*\.[^.]*\(\.uk\)\{0,1\}$//' | awk '{ printf ("\033k%s\033\\", $NF) }'
-
-dbg "Done."
+# ウィンドウタイトル変更
+echo "\033k$TITLE\033\\"
