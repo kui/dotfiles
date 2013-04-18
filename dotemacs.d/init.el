cf44352804c19fde9b85e5566eb7e5abe284e643
Keiichiro Ui <keiichiro.ui@gmail.com>
2013-04-18 14:20:51 +0900

    update

--- dotemacs.d/init.el
+++ dotemacs.d/init.el
@@ -834,6 +834,21 @@ create *scratch* if it did not exists"
 (when (kui/autoload-if-exist 'typescript-mode "TypeScript")
   (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode)))
 
+(when (kui/autoload-if-exist 'multi-web-mode "multi-web-mode")
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . multi-web-mode))
+  (eval-after-load "multi-web-mode"
+    '(let nil
+       (setq mweb-default-major-mode 'html-mode)
+       (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                         (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>"
+                                  "</script>")
+                         (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+       (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+       (multi-web-global-mode 1)
+       )))
+
+
+
 ;; -------------------------------------------------------------------------
 ;; 色とか
 (when (kui/package-require 'color-theme nil nil t)
