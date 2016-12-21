fbc22e572ba57b032fa5e334c0f3c6f49d894271
Keiichiro Ui <keiichiro.ui@gmail.com>
2016-12-21 21:24:54 +0900

    Update mouse configs

--- installs/x-startup.sh
+++ installs/x-startup.sh
@@ -2,8 +2,11 @@
 
 xset m 4/1 10
 
-xinput set-prop 'ELECOM ELECOM BlueLED Mouse' 'Device Accel Constant Deceleration' 2
+xinput set-prop 'ELECOM ELECOM BlueLED Mouse' 'Device Accel Constant Deceleration' 4
 xinput set-prop 'ELECOM ELECOM BlueLED Mouse' 'Evdev Wheel Emulation' 1
 xinput set-prop 'ELECOM ELECOM BlueLED Mouse' 'Evdev Wheel Emulation Button' 2
 xinput set-prop 'ELECOM ELECOM BlueLED Mouse' 'Evdev Wheel Emulation Axes' 6 7 4 5
 xinput set-prop 'ELECOM ELECOM BlueLED Mouse' 'Evdev Wheel Emulation Inertia' 5
+
+xinput set-prop 'Logitech Unifying Device. Wireless PID:4101' 'Device Accel Constant Deceleration' 1
+xinput set-button-map 'Logitech Unifying Device. Wireless PID:4101' 1 2 3 5 4 7 6
