292777c2091f604b6514bd5402a16a066aa3b280
Keiichiro Ui <keiichiro.ui@gmail.com>
2016-12-23 18:02:50 +0900

    Add limit play function

--- bin/play_agqr.sh
+++ bin/play_agqr.sh
@@ -2,7 +2,13 @@
 set -eu
 
 # 自宅サーバでagqrを再生する
+# aplay require "audio" group
 
-rtmpdump -r rtmp://fms-base1.mitene.ad.jp/agqr/aandg22 --live -o - | \
+if [ $# -ne 1 ]; then
+  echo "Require integer at first argument"
+  exit 1
+fi
+
+rtmpdump -r rtmp://fms-base1.mitene.ad.jp/agqr/aandg22 --live --stop $1 -o - | \
     ffmpeg -i - -vn -f s16le -ar 48k -ac 2 - | \
     aplay -f dat -D plughw:1,0
