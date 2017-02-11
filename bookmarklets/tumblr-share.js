b18a15e4299d6583de6d693bdfc94dd897f1ff90
Keiichiro Ui <keiichiro.ui@gmail.com>
2017-02-11 20:05:49 +0900

    Share selected imgs

--- bookmarklets/tumblr-share.js
+++ bookmarklets/tumblr-share.js
@@ -1,4 +1,8 @@
 ((window, document) => {
+  function uniq(array) {
+    return Array.from(new Set(array));
+  }
+
   const encode = window.encodeURIComponent;
   const $ = document.querySelector.bind(document);
   const $$ = document.querySelectorAll.bind(document);
@@ -12,18 +16,49 @@
     caption: encode(`<a href="${url}">${title}</a>`),
   };
 
-  // extract imgage
-  // twitter
-  const query = "#permalink-overlay [data-element-context='platform_photo_card'] img";
-  const imgs = Array.from($$(query))
-      .map(i => i.src + ":orig");
-  if (imgs.length !== 0) {
-    q.posttype = "photo";
-    q.content = imgs.map(encode).join(",");
-  }
+  const contentExtractors = [
+    () => {
+      const selection = window.getSelection();
+      if (selection.rangeCount === 0) return;
+
+      let imgs = [];
+      for (const e of selection.getRangeAt(0).commonAncestorContainer.querySelectorAll("*")) {
+        if (!selection.containsNode(e, true)) continue;
+        if (e.tagName === "IMG" && e.src) {
+          imgs.push(e.src);
+        }
+        const style = window.getComputedStyle(e);
+        const urlMatch = /^url\(["']?([^"']+)["']?\)$/.exec(style.backgroundImage);
+        if (urlMatch) {
+          imgs.push(urlMatch[1]);
+        }
+      }
+      if (imgs.length === 0) return;
+      imgs = uniq(imgs);
+      console.log("imgs", imgs);
+
+      q.posttype = "photo";
+      q.content = imgs.map(encode).join(",");
+    },
+    () => {
+      const query = "#permalink-overlay [data-element-context='platform_photo_card'] img";
+      let imgs = Array.from($$(query)).map(i => i.src + ":orig");
+      if (imgs.length === 0) return;
+      imgs = uniq(imgs);
+      console.log("imgs", imgs);
+
+      q.posttype = "photo";
+      q.content = uniq(imgs).map(encode).join(",");
+    },
+  ];
+  contentExtractors.some((e) => {
+    e();
+    return q.content != null;
+  });
 
   const tumblr = "https://www.tumblr.com/widgets/share/tool?"
         +Object.entries(q).map(e => e.join("=")).join("&");
+  const screen = window.screen;
   const height = screen && screen.height || 600;
   window.open(tumblr, null, `height=${height},width=540`);
 
