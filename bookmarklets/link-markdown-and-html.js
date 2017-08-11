948c23f2b37e8aff4e85239b2ba1a5fc9812c6c0
Keiichiro Ui <keiichiro.ui@gmail.com>
2017-08-11 20:06:30 +0900

    Avoid window.open with "data:" scheme because it is not supported by latest chromes

--- bookmarklets/link-markdown-and-html.js
+++ bookmarklets/link-markdown-and-html.js
@@ -3,7 +3,7 @@
   const title = document.title || link;
   const md = document.title ? `[${title}](${link})` : `<${link}>`;
   const content = `<a href="${link}">${title}</a><br>${md}`;
-  const wrapperStyle = "padding:5px; line-height:1.3em; font-size:120%; border:solid 1px black;"
-  const html = `<div contenteditable style="${wrapperStyle}" autofocus>${content}</div>`;
-  window.open(`data:text/html;charset=utf-8,${html}`,'_blank')
+  const wrapperStyle = "width: 300px; padding:5px; line-height:1.3em; font-size:120%; border:solid 1px black; background-color: white; position: fixed; top: 50%; left: calc(50% - 150px);"
+  const html = `<div contenteditable style="${wrapperStyle}" autofocus id="kui_links">${content}</div>`;
+  document.body.insertAdjacentHTML('beforeend', html);
 })();
