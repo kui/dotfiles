94f11927601b8f6129bab6b6fc1c358776d5543d
Keiichiro Ui <keiichiro.ui@gmail.com>
2019-02-21 14:42:20 +0900

    Define new function for simplify

--- installs/karabiner/kui-karabiner.js
+++ installs/karabiner/kui-karabiner.js
@@ -74,18 +74,11 @@ module.exports = {
           bundle_identifiers: terminals
         }
       ], [
-        {
-          from: fromKey('any? + left_option'),
-          to: [ sendKey('left_command') ],
-        },
-        {
-          from: fromKey('any? + left_command'),
-          to: [ sendKey('left_option') ],
-        },
-        {
-          from: fromKey('option + shift? + tab'),
-          to: [ sendKey('command + tab') ],
-        },
+        // bindKey('any? + left_option', 'left_command'),
+        bindKey('left_option', 'left_command'),
+        // bindKey('any? + left_command', 'left_option'),
+        bindKey('left_command', 'left_option'),
+        bindKey('option + shift? + tab', 'command + tab'),
       ]),
     },
 
@@ -98,26 +91,11 @@ module.exports = {
           bundle_identifiers: webBrowsers,
         },
       ], [
-        {
-          from: fromKey('control + shift? + t'),
-          to: [ sendKey('command + t') ],
-        },
-        {
-          from: fromKey('control + shift? + l'),
-          to: [ sendKey('command + l') ],
-        },
-        {
-          from: fromKey('control + shift? + r'),
-          to: [ sendKey('command + r') ],
-        },
-        {
-          from: fromKey('command + shift + period'),
-          to: [ sendKey('command + down_arrow') ],
-        },
-        {
-          from: fromKey('command + shift + comma'),
-          to: [ sendKey('command + up_arrow') ],
-        },
+        bindKey('control + shift? + t', 'command + t'),
+        bindKey('control + shift? + l', 'command + l'),
+        bindKey('control + shift? + r', 'command + r'),
+        bindKey('command + shift + period', 'command + down_arrow'),
+        bindKey('command + shift + comma', 'command + up_arrow'),
       ]),
     },
 
@@ -130,38 +108,22 @@ module.exports = {
           bundle_identifiers: ides,
         },
       ], [
-        {
-          from: fromKey('command + x'),
-          to: [ sendKey('command + 3') ],
-        },
-        {
-          from: fromKey('command + i'),
-          to: [ sendKey('command + e') ],
-        },
-        {
-          from: fromKey('control + o'),
-          to: [ sendKey('f3') ],
-        },
-        {
-          from: fromKey('control + s'),
-          to: [ sendKey('command + j') ],
-        },
-        {
-          from: fromKey('command + slash'),
-          to: [ sendKey('control + spacebar') ],
-        },
-        {
-          conditions: [{
+        bindKey('command + x', 'command + 3'),
+        bindKey('command + i', 'command + e'),
+        bindKey('control + o', 'f3'),
+        bindKey('control + s', 'command + j'),
+        bindKey('command + slash', 'control + spacebar'),
+        Object.assign(
+          bindKey('control + f', [
+            'command + shift + r',
+            unsetMode()
+          ]),
+          { conditions: [{
             type: 'variable_if',
             name: 'mode',
             value: Modes.Cx,
-          }],
-          from: fromKey('control + f'),
-          to: [
-            sendKey('command + shift + r'),
-            unsetMode(),
-          ],
-        },
+          }]},
+        )
       ]),
     },
 
@@ -175,10 +137,7 @@ module.exports = {
         },
       ], [
         // Mark mode
-        {
-          from: fromKey('control + spacebar'),
-          to: [ setMode(Modes.Mark) ],
-        },
+        bindKey('control + spacebar', setMode(Modes.Mark)),
         ...conditionsGroup(
           [{
             type: 'variable_if',
@@ -186,58 +145,22 @@ module.exports = {
             value: Modes.Mark,
           }],
           [
-            {
-              from: fromKey('control + b'),
-              to: [ sendKey('shift + left_arrow') ],
-            },
-            {
-              from: fromKey('command + b'),
-              to: [ sendKey('shift + option + left_arrow') ],
-            },
-            {
-              from: fromKey('control + f'),
-              to: [ sendKey('shift + right_arrow') ],
-            },
-            {
-              from: fromKey('command + f'),
-              to: [ sendKey('shift + option + right_arrow') ],
-            },
-            {
-              from: fromKey('control + n'),
-              to: [ sendKey('shift + down_arrow') ],
-            },
-            {
-              from: fromKey('control + p'),
-              to: [ sendKey('shift + up_arrow') ],
-            },
-            {
-              from: fromKey('control + v'),
-              to: [ sendKey('shift + page_down') ],
-            },
-            {
-              from: fromKey('command + v'),
-              to: [ sendKey('shift + page_up') ],
-            },
-            {
-              from: fromKey('control + a'),
-              to: [ sendKey('shift + command + left_arrow') ],
-            },
-            {
-              from: fromKey('control + e'),
-              to: [ sendKey('shift + command + right_arrow') ],
-            },
-            {
-              from: fromKey('control + g'),
-              to: [ unsetMode() ],
-            },
+            bindKey('control + b', 'shift + left_arrow'),
+            bindKey('command + b', 'shift + option + left_arrow'),
+            bindKey('control + f', 'shift + right_arrow'),
+            bindKey('command + f', 'shift + option + right_arrow'),
+            bindKey('control + n', 'shift + down_arrow'),
+            bindKey('control + p', 'shift + up_arrow'),
+            bindKey('control + v', 'shift + page_down'),
+            bindKey('command + v', 'shift + page_up'),
+            bindKey('control + a', 'shift + command + left_arrow'),
+            bindKey('control + e', 'shift + command + right_arrow'),
+            bindKey('control + g', unsetMode()),
           ]
         ),
 
         // C-x Prefix Bindings
-        {
-          from: fromKey('control + x'),
-          to: [ setMode(Modes.Cx) ],
-        },
+        bindKey('control + x', setMode(Modes.Cx)),
         ...conditionsGroup(
           [{
             type: 'variable_if',
@@ -245,42 +168,28 @@ module.exports = {
             value: Modes.Cx,
           }],
           [
-            {
-              from: fromKey('h'),
-              to: [
-                sendKey('command + a'),
-                unsetMode(),
-              ],
-            },
-            {
-              from: fromKey('control + s'),
-              to: [
-                sendKey('command + s'),
-                unsetMode(),
-              ],
-            },
-            {
-              from: fromKey('control + f'),
-              to: [
-                sendKey('command + o'),
-                unsetMode(),
-              ],
-            },
-            {
-              from: fromKey('control + c'),
-              to: [
-                sendKey('command + q'),
-                unsetMode(),
-              ],
-            },
-            {
-              from: fromKey('control + g'),
-              to: [ unsetMode() ],
-            },
+            bindKey('h', [
+              sendKey('command + a'),
+              unsetMode(),
+            ]),
+            bindKey('control + s', [
+              'command + s',
+              unsetMode(),
+            ]),
+            bindKey('control + f', [
+              'command + o',
+              unsetMode(),
+            ]),
+            bindKey('control + c', [
+              'command + q',
+              unsetMode(),
+            ]),
+            bindKey('control + g', unsetMode()),
 
             // Escape C-x mode if unknown key
             // TODO Fix that this expression escapes mode if modifier key was pressed.
             {
+              type: 'basic',
               from: { any: 'key_code' },
               to: [ unsetMode() ],
             },
@@ -288,140 +197,68 @@ module.exports = {
         ),
 
         // Delete (C-d, C-h)
-        {
-          from: fromKey('control + d'),
-          to: [
-            sendKey('delete_forward'),
-            unsetMode(),
-          ],
-        },
-        {
-          from: fromKey('control + h'),
-          to: [
-            sendKey('delete_or_backspace'),
-            unsetMode(),
-          ],
-        },
+        bindKey('control + d', [
+          'delete_forward',
+          unsetMode(),
+        ]),
+        bindKey('control + h', [
+          'delete_or_backspace',
+          unsetMode(),
+        ]),
 
         // Tab (C-i)
-        {
-          from: fromKey('control + i'),
-          to: [ sendKey('tab') ],
-        },
+        bindKey('control + i', 'tab'),
 
         // Esc (C-[)
-        {
-          conditions: [{
+        Object.assign(
+          bindKey('control + open_bracket', 'escape'),
+          { conditions: [{
             keyboard_types: [ 'ansi', 'iso' ],
             type: 'keyboard_type_if'
-          }],
-          from: fromKey('control + open_bracket'),
-          to: [ sendKey('escape') ],
-        },
-        {
-          conditions: [{
+          }]}
+        ),
+        Object.assign(
+          bindKey('control + close_bracket', 'escape'),
+          { conditions: [{
             keyboard_types: [ 'jis' ],
             type: 'keyboard_type_if'
-          }],
-          from: fromKey('control + close_bracket'),
-          to: [ sendKey('escape') ],
-        },
-        {
-          from: fromKey('control + g'),
-          to: [ sendKey('escape') ],
-        },
+          }],}
+        ),
+        bindKey('control + g', 'escape'),
 
         // Cursor Moves (C-n, C-p, C-f, C-b, M-f, M-b, C-a, C-e)
-        {
-          from: fromKey('control + b'),
-          to: [ sendKey('left_arrow') ],
-        },
-        {
-          from: fromKey('command + b'),
-          to: [ sendKey('option + left_arrow') ],
-        },
-        {
-          from: fromKey('control + f'),
-          to: [ sendKey('right_arrow') ],
-        },
-        {
-          from: fromKey('command + f'),
-          to: [ sendKey('option + right_arrow') ],
-        },
-        {
-          from: fromKey('control + n'),
-          to: [ sendKey('down_arrow') ],
-        },
-        {
-          from: fromKey('control + p'),
-          to: [ sendKey('up_arrow') ],
-        },
-        {
-          from: fromKey('control + a'),
-          to: [ sendKey('command + left_arrow') ],
-        },
-        {
-          from: fromKey('control + e'),
-          to: [ sendKey('command + right_arrow') ],
-        },
+        bindKey('control + b', 'left_arrow'),
+        bindKey('command + b', 'option + left_arrow'),
+        bindKey('control + f', 'right_arrow'),
+        bindKey('command + f', 'option + right_arrow'),
+        bindKey('control + n', 'down_arrow'),
+        bindKey('control + p', 'up_arrow'),
+        bindKey('control + a', 'command + left_arrow'),
+        bindKey('control + e', 'command + right_arrow'),
 
         // Page Up/Down
-        {
-          from: fromKey('control + v'),
-          to: [ sendKey('page_down') ],
-        },
-        {
-          from: fromKey('command + v'),
-          to: [ sendKey('page_up') ],
-        },
+        bindKey('control + v', 'page_down'),
+        bindKey('command + v', 'page_up'),
 
         // Cut/Copy/Paste
-        {
-          from: fromKey('control + w'),
-          to: [
-            sendKey('command + x'),
-            unsetMode(),
-          ],
-        },
-        {
-          from: fromKey('command + w'),
-          to: [
-            sendKey('command + c'),
-            unsetMode(),
-          ],
-        },
-        {
-          from: fromKey('control + y'),
-          to: [ sendKey('command + v') ],
-        },
+        bindKey('control + w', [
+          'command + x',
+          unsetMode(),
+        ]),
+        bindKey('command + w', [
+          'command + c',
+          unsetMode(),
+        ]),
+        bindKey('control + y', 'command + v'),
+        bindKey('control + k', [
+          'shift + command + right_arrow',
+          'command + x',
+          unsetMode(),
+        ]),
 
-        // Kill Line
-        {
-          from: fromKey('control + k'),
-          to: [
-            sendKey('shift + command + right_arrow'),
-            sendKey('command + x'),
-            unsetMode(),
-          ],
-        },
-
-        // Search
-        {
-          from: fromKey('control + s'),
-          to: [ sendKey('command + f') ],
-        },
-
-        // Undo
-        {
-          from: fromKey('control + slash'),
-          to: [ sendKey('command + z') ],
-        },
-
-        // M-k
-        {
-          from: fromKey('command + k'),
-          to: [ sendKey('command + w') ],
-        }
+        bindKey('control + s', 'command + f'),
+        bindKey('control + slash', 'command + z'),
+        bindKey('command + k', 'command + w'),
       ]),
     },
   ]
@@ -429,10 +266,44 @@ module.exports = {
 
 function conditionsGroup(conditions, manipulators) {
   return manipulators.map((m) => {
-    m.type = 'basic';
     m.conditions = (m.conditions || []).concat(conditions);
     return m;
   });
+}
+
+function bindKey(fromKeyStr, secondArg) {
+  const events = (() => {
+    const t = typeString(secondArg);
+
+    if (t === 'String') {
+      return [ sendKey(secondArg) ];
+    }
+
+    if (t === 'Array') {
+      return secondArg.map((a) => {
+        if (typeString(a) === 'String') {
+          return sendKey(a);
+        }
+        return a;
+      });
+    }
+
+    return [ secondArg ];
+  })();
+
+  return {
+    type: 'basic',
+    from: fromKey(fromKeyStr),
+    to: events,
+  };
+}
+
+function typeString(o) {
+  return Object
+    .prototype
+    .toString
+    .apply(o)
+    .replace(/^\[object (.*)\]$/, (_, m) => m);
 }
 
 // Use in "from"
