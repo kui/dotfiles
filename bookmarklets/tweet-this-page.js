9ab7e5408f4ef65ba6de7df0a0dae4050f3ee023
Keiichiro Ui <keiichiro.ui@gmail.com>
2025-03-16 14:56:38 +0900

    Add a bookmarklet

---
+++ bookmarklets/tweet-this-page.js
@@ -0,0 +1,4 @@
+const selection = window.getSelection().toString();
+const prefix = selection.length === 0 ? '' : `"${selection}" -`;
+const tweet = `${prefix} ${document.title} ${location.href}`;
+window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweet), '_blank');
