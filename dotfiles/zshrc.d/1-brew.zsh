138a75f99fadcddda20714642d49385b433093ff
Keiichiro Ui <oshigoto@example.com>
2025-06-23 15:23:04 +0900

    Follow the brew guide

--- dotfiles/zshrc.d/1-brew.zsh
+++ dotfiles/zshrc.d/1-brew.zsh
@@ -1,15 +1,19 @@
+if ! has_command brew && [[ -d /opt/homebrew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 has_command brew || return
 
 path=(
     $HOME/.{settings,dotfiles}/bin(N-/)
     $HOME/.local/bin(N-/)
-    $(brew --prefix)/bin(N-/)
+    #$(brew --prefix)/bin(N-/)
     $(brew --prefix)/opt/*/libexec/gnubin
     $path
 )
 
 manpath=(
-    $(brew --prefix)/share/man(N-/)
+    #$(brew --prefix)/share/man(N-/)
     $(brew --prefix)/opt/*/libexec/gnuman
     $manpath
 )
